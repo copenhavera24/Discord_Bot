85230faf6386c5837879f2f27e91d753f9fe3f0d
Austin <copenhavera24@gmail.com>
2026-02-07 16:13:38 -0500

    still a small issue with my own custom cog

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import nextcord
 from nextcord.ext import commands
 import os
 
+from nhlpy import NHLClient
 from dotenv import load_dotenv
 from nextcord.ext import commands
 
@@ -34,6 +35,39 @@ if(os.getenv("TARGET_ENV") == "dev"):
     async def test(ctx, args):
         await ctx.send(args)
 
+
+    class Hockey(commands.Cog):
+        def __init__(self, bot):
+            self.bot = bot
+            self._last_member = None
+
+        @commands.command()
+        async def games(self, ctx):
+            games = client.schedule.daily_schedule()
+            await ctx.send(games)
+
+    class Greetings(commands.Cog):
+        def __init__(self, bot):
+            self.bot = bot
+            self._last_member = None
+
+        @commands.Cog.listener()
+        async def on_member_join(self, member):
+            channel = member.guild.system_channel
+            if channel is not None:
+                await channel.send(f'Welcome {member.mention}!')
+
+        @commands.command()
+        async def hello(self,ctx, *, member: nextcord.Member = None):
+            member = member or ctx.author
+            if self._last_member is None or self._last_member != member.id:
+                await ctx.send(f'Hello {member.mention}!')
+            else:
+                await ctx.send(f'Hello {member.mention}... This seems familiar.')
+            self._last_member = member
+
+
+    bot.add_cog(Greetings(bot))
     bot.run(DISCORD_KEY)
 elif(os.getenv("TARGET_ENV") == "prod"):
     TESTING_GUILD_ID = os.getenv('TESTING_GUILD_ID')
